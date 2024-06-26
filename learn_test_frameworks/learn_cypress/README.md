bc6e0293baa847a874e8548e9bc8f93ae100caee
eliaschikwanda <eliaschikwanda>
2024-06-26 14:42:28 +0200

    Finished the first video on object locator on a webpage and how to open a url

--- learn_test_frameworks/learn_cypress/README.md
+++ learn_test_frameworks/learn_cypress/README.md
@@ -1,2 +1,7 @@
 Hello folks, I'm learning (I learnt) cypress using a YouTube channel (https://www.youtube.com/playlist?list=PLhW3qG5bs-L9LTfxZ5LEBiM1WFfvX3dJo) by 
 Raghav. The tutorial has been a useful intro to cypress and I will keep anything I wanna remeber on this page as my cheatsheet.
+
+<b>Assertions in Cypress</b>
+<p>We have two types of assertions in cypress Explicit and Implicit</p>
+<p>For explicit we use the commands 'and()' and 'should()'</p>
+<p>For implicit we use the command 'assert()' and 'expect()'</p>
--- learn_test_frameworks/learn_cypress/cypress/e2e/mytests/test1.cy.js
+++ learn_test_frameworks/learn_cypress/cypress/e2e/mytests/test1.cy.js
@@ -1,1 +1,8 @@
 /// <reference types = "cypress"/>
+
+it('Google Search', () => { // 'Google Search' is the title of the test.
+    cy.visit('https://google.com') // Use cy as a command as it contains the cypress methods use visit with the url as the argument.
+    cy.get('#APjFqb').type('Who is Elias Chikwanda?{Enter}') // Get the identifier on the search bar and populate it the {Enter} Simulate the keyboard and press enter.
+    //cy.get('.FPdoLc > center > .gNO89b').click() --> one way to select the button and press enter
+
+})
