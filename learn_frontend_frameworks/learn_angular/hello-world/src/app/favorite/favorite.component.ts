4bbef4e281ca3753275bcf3a0bc4f5b9bb388512
eliaschikwanda <chikwandaelias@gmail.com>
2024-07-10 08:29:07 +0200

    Last docker lesson

--- learn_frontend_frameworks/learn_angular/hello-world/src/app/favorite/favorite.component.ts
+++ learn_frontend_frameworks/learn_angular/hello-world/src/app/favorite/favorite.component.ts
@@ -7,6 +7,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 
 // The @Input property is used to mark fields as input properties
+// The data is in available in the post and the field can be changed
+// using by binding the varibale in the favorite selector.
 export class FavoriteComponent implements OnInit {
   @Input() isFavorite = false;
   
