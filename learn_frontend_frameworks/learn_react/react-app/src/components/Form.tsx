05e25fbdce241188344e4a3b38e7dfd82015dab7
Elias Chikwanda <chikwandaelias@gmail.com>
2024-07-14 08:42:49 +0200

    Completed reactForm validation using custom hookForms, now about to start validation with zod

--- learn_frontend_frameworks/learn_react/react-app/src/components/Form.tsx
+++ learn_frontend_frameworks/learn_react/react-app/src/components/Form.tsx
@@ -1,5 +1,11 @@
 import { FormEvent, useRef, useState } from "react";
-import { convertToObject } from "typescript";
+import { Field, FieldValue, FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+
+interface FormData {
+  name: string;
+  age: number;
+}
 
 export const Form = () => {
   // const nameRef = useRef<HTMLInputElement>(null) // A good practice to initialize null
@@ -17,32 +23,50 @@ export const Form = () => {
   //     }
   //     console.log(person)
   // }
-  const [person, setPerson] = useState({
-    name: "",
-    age: 0,
-  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    console.log(person);
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
   };
 
+  // const [person, setPerson] = useState({
+  //   name: "",
+  //   age: 0,
+  // });
+
+  // const handleSubmit = (event: FormEvent) => {
+  //   event.preventDefault();
+  //   console.log(person);
+  // };
+
   return (
-    <form action="" onSubmit={handleSubmit}>
+    <form action="" onSubmit={handleSubmit(onSubmit)}>
+      {" "}
+      {/* When you submit the data react will call that function */}
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
         {/* <input ref={nameRef} id="name" type="text" className="form-control" /> */}
         <input
-          value={person.name}
-          onChange={(event) =>
-            setPerson({ ...person, name: event.target.value })
-          }
+          {...register("name", { required: true, minLength: 3 })}
           id="name"
           type="text"
           className="form-control"
         />
+        {/* the question mark is optional chaining */}
+        {errors.name?.type === "required" && (
+          <p className="text-danger">The name field is required</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">
+            The name field must be at least 3 characters
+          </p>
+        )}
       </div>
       {/* div.mb-3>label.form-label+input[type=number].form-control to get that something below */}
       <div className="mb-3">
@@ -51,10 +75,7 @@ export const Form = () => {
         </label>
         {/* <input ref={ageRef} id="age" type="number" className="form-control" /> */}
         <input
-          value={person.age}
-          onChange={(event) =>
-            setPerson({ ...person, age: parseInt(event.target.value) })
-          }
+          {...register("age")}
           id="age"
           type="number"
           className="form-control"
