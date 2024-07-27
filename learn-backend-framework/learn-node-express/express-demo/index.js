fbd860f36649cc0dabd1b1a99b28ed52f8618a98
Elias Chikwanda <chikwandaelias@gmail.com>
2024-07-27 10:17:50 +0200

    Handling PUT requests.

--- learn-backend-framework/learn-node-express/express-demo/index.js
+++ learn-backend-framework/learn-node-express/express-demo/index.js
@@ -10,6 +10,7 @@ const courses = [
     {id: 3, name: 'course3'},
 ]
 
+
 app.get('/', (req, res) => {
     res.send('Hello World');    
 }); // Use to handle http get request.
@@ -18,6 +19,7 @@ app.get('/api/courses', (req, res) => {
     res.send(courses);
 });
 
+// HTTP get request
 app.get('/api/courses/:id', (req, res) => {
     // To read the passed parameter we use req.params.{passed-param-name}
     const course = courses.find( c => c.id === parseInt(req.params.id));
@@ -25,15 +27,13 @@ app.get('/api/courses/:id', (req, res) => {
     res.send(course);
 });
 
+// HTTP post request
 app.post('/api/courses', (req, res) => {
-    const schema = {
-        name: Joi.string().min(3).required(),
-    };
-    const result = Joi.validate(req.body, schema);
-    if (result.error) {
-        res.status(400).send(res.error.details[0].message);
+    const {error} = validateCourse(req.body);
+    if (error) {
+        res.status(400).send(error.details[0].message);
         return;
-    }
+    } 
     const course = {
         id: courses.length + 1,
         name: req.body.name, // Enable the passing of jason object to use this feature
@@ -41,6 +41,34 @@ app.post('/api/courses', (req, res) => {
     courses.push(course);
     res.send(course);
 });
+
+// HTTP put request
+app.put('/api/courses/:id', (req, res) => {
+    // Look up the course
+    // if not existing return 404
+    const course = courses.find( c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given ID was not found');
+
+    // validate
+    // If invalid, return 400 -Bad request
+    const {error} = validateCourse(req.body);
+    
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+    // Update the course
+    // Return the updated courses.
+    course.name = req.body.name;
+    res.send(course);
+});
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required(),
+    };
+    return Joi.validate(course, schema);
+}
 
 const port = process.env.PORT || 3000;
 
