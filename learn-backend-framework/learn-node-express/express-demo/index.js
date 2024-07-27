fd574516961b0dd334735e07668883d8b4c0d027
Elias Chikwanda <chikwandaelias@gmail.com>
2024-07-27 09:24:23 +0200

    Route Parameters

--- learn-backend-framework/learn-node-express/express-demo/index.js
+++ learn-backend-framework/learn-node-express/express-demo/index.js
@@ -9,6 +9,11 @@ app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3]);
 });
 
+app.get('/api/posts/:year/:month', (req, res) => {
+    // To read the passed parameter we use req.params.{passed-param-name}
+    res.send(req.params);
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
