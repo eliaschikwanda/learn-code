9082f2af6a4d6589f7ba2a4f0b60c8037a3c29ff
Elias Chikwanda <chikwandaelias@gmail.com>
2024-07-28 08:02:33 +0200

    Running promises in paraellel

--- learn-backend-framework/learn-node-express/async-demo/promise-api.js
+++ learn-backend-framework/learn-node-express/async-demo/promise-api.js
@@ -1,7 +1,26 @@
 // Creating an already resolved promise
-const p =Promise.resolve({id: 1});
-p.then((res) => {console.log(res)});
+const p1 =Promise.resolve({id: 1});
+p1.then((res) => {console.log(res)});
 
 // Creating an already rejected promise =
-const p1 = Promise.reject(new Error('Error message..'));
-p1.catch((err) => {console.log(err.message)});
+const p2 = Promise.reject(new Error('Error message..'));
+p2.catch((err) => {console.log(err.message, '\n')});
+
+// Running promises in Parallel
+const p3 = new Promise((resolve, result) => {
+    setTimeout(() => {
+        console.log('Async operation 1...');
+        resolve(1);
+    }, 2000);
+});
+
+const p4 = new Promise((resolve, result) => {
+    setTimeout(() => {
+        console.log('Async operation 2...');
+        resolve(2);
+    }, 2000);
+});
+
+// Kick off all synchronous processes
+Promise.all([p3, p4])
+ .then((result) => {console.log(result)});
