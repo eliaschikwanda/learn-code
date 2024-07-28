4a4eb227a779c125cfc1a65e166c24068270d27e
Elias Chikwanda <chikwandaelias@gmail.com>
2024-07-28 07:44:24 +0200

    Changing from callbacks and promises

--- learn-backend-framework/learn-node-express/async-demo/index.js
+++ learn-backend-framework/learn-node-express/async-demo/index.js
@@ -1,22 +1,42 @@
 console.log('Before');
-getUser(1, (user) => {
-    console.log('User', user);
-    getRepositories(user.gitHubUsername, (repos) => {
-        console.log('Repos',repos);
-    })
-});
+// getUser(1, (user) => { 
+//     console.log('User', user);
+//     getRepositories(user.gitHubUsername, (repos) => {
+//         console.log('Repos',repos);
+//     })
+// });
+
+getUser(1)
+  .then(user => getRepositories(user.gitHubUsername))
+  .then(repos => getCommit(repos[0]))
+  .then(commits => console.log('Commits: ', commits))
+  .catch(err => console.log(err.message));
+
 console.log('After');
 
-function getUser(id, callback) {
-    setTimeout(() => {
-        console.log('Reading a user from a database... ');
-        callback({id: id, gitHubUsername: 'elias'});
-    }, 2000);
+function getUser(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Reading a user from a database... ');
+            resolve({id: id, gitHubUsername: 'elias'});
+        }, 2000);
+    });
 };
 
-function getRepositories(username, callback) {
-    setTimeout(() => {
-        console.log('Getting data from github API... ')
-        callback(['repo', 'repo2', 'repo3']);
-    }, 2000);
+function getRepositories(username) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Getting data from github API... ')
+            resolve(['repo1', 'repo2', 'repo3']);
+        }, 2000);
+    });
+}
+
+function getCommit(repo) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Calling GitHub API...');
+            resolve(['commit'])
+        }, 2000);
+    });
 }
