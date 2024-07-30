d66095d2a4bcbdeaa9fe4415df01e94176232eec
eliaschikwanda <chikwandaelias@gmail.com>
2024-07-30 08:34:08 +0200

    Next.js uncommitted changes

--- learn_frontend_frameworks/learn_next.js/next-app/app/products/[[...slug]]/page.tsx
+++ learn_frontend_frameworks/learn_next.js/next-app/app/products/[[...slug]]/page.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 
 interface Props {
-    params: {slug: string[]}
+    params: {slug: string[]};
+    searchParams: {sortOrder: string}
 }
 
-const ProductPage = ({params: {slug}} : Props) => {
+const ProductPage = ({params: {slug}, searchParams: {sortOrder}} : Props) => {
   if (!slug) return <div>Product Page </div>
 
   return (
     <div>
-        {slug.map((parameter) => <p key={parameter}>{parameter}</p>)}
+        {slug.map((parameter) => <p key={parameter}>{parameter} {sortOrder}</p>)}
     </div>
   )
 }
--- learn_frontend_frameworks/learn_next.js/next-app/app/users/UserTable.tsx
+++ learn_frontend_frameworks/learn_next.js/next-app/app/users/UserTable.tsx
@@ -1,4 +1,10 @@
+import Link from 'next/link';
 import React from 'react'
+import { sort } from 'fast-sort';
+
+interface Props {
+    sortOrder: string;
+}
 
 interface User {
     id: number;
@@ -6,15 +12,20 @@ interface User {
     email: string;
   }
 
-const UserTable = async () => {
+const UserTable = async ({sortOrder}: Props) => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
-  const users: User[] = await res.json();
+    const users: User[] = await res.json();
+    sort(users).asc(sortOrder === 'email' ? (user) => user.email : (user) => user.name)
   return (
     <table className='table table-bordered'>
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Email</th>
+            <th>
+                <Link href={'/users?sortOrder=name'}>Name</Link>
+            </th>
+            <th>
+                <Link href={'/users?sortOrder=email'}>Email</Link>
+            </th>
           </tr>
         </thead>
         <tbody>
