0aa9a6d2f4f2b21b63f67454dbfd89d2f21dc684
eliaschikwanda <chikwandaelias@gmail.com>
2024-07-30 13:28:49 +0200

    Created a job posting mapping and created the job successfully.

--- learn-backend-framework/learn-spring-boot/firstjobapp/src/main/java/com/jphanos/firstjobapp/job/JobController.java
+++ learn-backend-framework/learn-spring-boot/firstjobapp/src/main/java/com/jphanos/firstjobapp/job/JobController.java
@@ -1,6 +1,8 @@
 package com.jphanos.firstjobapp.job;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -14,4 +16,27 @@ public class JobController {
     public List<Job> findAll() {
         return jobs;
     }
+
+    @PostMapping("/jobs")
+    public String createJob(@RequestBody Job job) {
+        jobs.add(job);
+        return "Job created successfully";
+    }
 }
+
+/*
+
+ GET /jobs: Get all jobs
+ GET /jobs/{id}: Get a specific job by ID
+ POST /jobs: Create a new job (request body should contain the job details)
+ DELETE /jobs/{id}: Delete a specific job by ID.
+ PUT /jobs/{id}: Update a specific job by ID (request body should contain the updated job)4
+
+ Example API URLs:
+ GET {base_url}/jobs
+ GET {base_url}/jobs/1
+ POST {base_urls}/jobs
+ DELETE {base_url}/jobs/1
+ PUT {base_url}/jobs/1
+
+*/
