39213fac46062fd63d343e676ae3e56f3153df89
eliaschikwanda <chikwandaelias@gmail.com>
2024-08-27 13:20:09 +0200

    Extended the User entity with the UserDetailsClass

---
+++ learn-backend-framework/learn-spring-boot/spring-security/src/main/java/com/jphanos/spring_security/user/Role.java
@@ -0,0 +1,5 @@
+package com.jphanos.spring_security.user;
+
+public enum Role {
+    USER, ADMIN
+}
--- learn-backend-framework/learn-spring-boot/spring-security/src/main/java/com/jphanos/spring_security/user/User.java
+++ learn-backend-framework/learn-spring-boot/spring-security/src/main/java/com/jphanos/spring_security/user/User.java
@@ -2,6 +2,12 @@ package com.jphanos.spring_security.user;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
@@ -10,7 +16,7 @@ import lombok.*;
 @AllArgsConstructor
 @Entity
 @Table(name = "_user") // Because the database already has a table name User it's best to give "_user" to your table name
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Integer id;
@@ -18,4 +24,37 @@ public class User {
     private String lastname;
     private String email;
     private String password;
+
+    @Enumerated(EnumType.STRING) // Telling Spring it is an enum
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
