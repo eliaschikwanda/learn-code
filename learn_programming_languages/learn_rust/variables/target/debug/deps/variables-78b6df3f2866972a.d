50493c1e0433b958e0d2d8220e850a07eb3c5867
eliaschikwanda <chikwandaelias@gmail.com>
2025-04-26 14:42:28 -0700

    The mutability of rust variables

---
+++ learn_programming_languages/learn_rust/variables/target/debug/deps/variables-78b6df3f2866972a.d
@@ -0,0 +1,5 @@
+/Users/elias/code/learn-code/learn_programming_languages/learn_rust/variables/target/debug/deps/variables-78b6df3f2866972a: src/main.rs
+
+/Users/elias/code/learn-code/learn_programming_languages/learn_rust/variables/target/debug/deps/variables-78b6df3f2866972a.d: src/main.rs
+
+src/main.rs:
---
+++ learn_programming_languages/learn_rust/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/elias/code/learn-code/learn_programming_languages/learn_rust/variables/target/debug/variables: /Users/elias/code/learn-code/learn_programming_languages/learn_rust/variables/src/main.rs
